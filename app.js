c42da3f1958c3e481de8bbd67e290778a87a8d05
Gabriel <gabriel.goldas@gmail.com>
2025-07-04 18:44:50 -0300

    start to implements the order function

--- app.js
+++ app.js
@@ -39,17 +39,18 @@ function renderList(status) {
 
         // select bought or pending
         const select = document.createElement('select')
-        // select.id = 'status'
         select.classList.add(item.status)
         select.name = 'status'
 
         const bought = document.createElement('option')
         bought.value = 'comprado'
         bought.innerText = 'Comprado'
+        bought.className = 'comprado'
 
         const pending = document.createElement('option')
         pending.value = 'pendente'
         pending.innerText = 'Pendente'
+        pending.className = 'pendente'
 
         // Marcar a opção atual
         if (item.status === 'comprado') {
@@ -67,8 +68,8 @@ function renderList(status) {
             items[index].status = select.value
             saveData()
 
-            select.classList.remove('pendente', 'comprado')
-            select.classList.add(select.value)
+            // select.classList.remove('pendente', 'comprado')
+            // select.classList.add(select.value)
 
             renderList()
         })
@@ -104,9 +105,11 @@ formAdd.addEventListener('submit', (ev) => {
 })
 
 function removeItem(index) {
-    items.splice(index, 1)
-    saveData()
-    renderList()
+    if (confirm(`Voce realmente deseja excluir o "${items[index].name} da sua lista?"`)) {
+        items.splice(index, 1)
+        saveData()
+        renderList()
+    }
 }
 
 btnClear.addEventListener('click', () => {
@@ -138,6 +141,54 @@ function filter() {
     }
 }
 
+const orderSelect = document.querySelector('.order-select')
+const aZ = document.querySelector('.a-z')
+const zA = document.querySelector('.z-a')
+const compradoOrder = document.querySelector('.comprado-order')
+const pendenteOrder = document.querySelector('.pendente-order')
+
+const selectedValue = orderSelect.value
+
+// Marcar a opção atual
+switch (selectedValue) {
+
+}
+
+
+select.appendChild(bought)
+select.appendChild(pending)
+
+// action on select
+select.addEventListener('change', () => {
+    items[index].status = select.value
+    saveData()
+
+    // select.classList.remove('pendente', 'comprado')
+    // select.classList.add(select.value)
+
+    renderList()
+})
+
+
+
+function order(orderOption) {
+    switch (orderOption.value) {
+        case 'a-z':
+            items.sort((a, b) => a.name.localeCompare(b.name))
+            break
+        case 'z-a':
+            items.sort((a, b) => b.name.localeCompare(a.name))
+            break
+        case 'comprado':
+            items.sort((a, b) => a.status.localeCompare(b.status))
+            break
+        case 'pendente':
+            items.sort((a, b) => b.status.localeCompare(a.status))
+    }
+
+    renderList()
+}
+
 btnSearch.addEventListener('click', () => filter())
 
 
@@ -145,4 +196,4 @@ btnSearch.addEventListener('click', () => filter())
 // Marcar como comprado - Salvar esse Estado no localStorage ✅
 // Contador de Itens - Mostras quantos items tem na lista, atualizando em tempo real ✅
 // Adicione filtros para items 'comprados' e 'pendentes' ✅
-// Permita ordenar alfabeticamente ou por status
+// Permita ordenar alfabeticamente ou por status 
